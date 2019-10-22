d58ae938a816698b32e5a4893c5ebed4d4c0076b
chirag Lodariya <chirag.lodariya@kevit.io>
2019-10-22 18:33:10 +0530

    added test cases

--- app/core/models.py
+++ app/core/models.py
@@ -9,7 +9,7 @@ class UserManager(BaseUserManager):
            raise ValueError('User must have an email address.')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
-        user.save(using=self.db)
+        user.save(using=self._db)
 
         return user
 
@@ -17,7 +17,7 @@ class UserManager(BaseUserManager):
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
-        user.save(using=self.db)
+        user.save(using=self._db)
 
         return user
 
@@ -28,6 +28,6 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
-    object = UserManager()
+    objects = UserManager()
 
     USERNAME_FIELD = 'email'
